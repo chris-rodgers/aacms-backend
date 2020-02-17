6f183fbe51319ed20526d7280865ae0050ca7df1
Chris Rodgers <crodgers@alternativeairlines.com>
2020-02-17 11:16:01 +0000

    Slight refactor

--- app/Aacms.php
+++ app/Aacms.php
@@ -3,10 +3,85 @@
 namespace App;
 
 use Symfony\Component\ErrorHandler\Error\FatalError;
+use Illuminate\Support\Facades\Storage;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Support\Facades\Blade;
 
 class Aacms
 {
-    public static function render($__php, $__data)
+    /**
+     * Return a list of all components available
+     */
+    public static function getComponentList(){
+        $res = [];
+        $components = Storage::disk('components')->files('');
+        
+        foreach ($components as $component) {
+            $content = Storage::disk('components')->get($component);
+            $name = explode(".", $component, 2)[0];
+            $object = YamlFrontMatter::parse($content);
+            preg_match_all('/\$(\w*-?>?\[?\'?"?\]?+)/', $object->body(), $variables);
+            $res[$name] = [
+                'fields' => $object->matter(),
+                'variableNames' =>  $variables[1]
+            ];
+        }
+        
+        return $res;
+    }
+
+    /**
+     * Rendering components as html
+     */
+    public static function render($components)
+    {
+        $rendered = '';
+
+        $fieldParsers = [
+            'RichText' => function () {
+                return 'rich text string';
+            }
+        ];
+
+        function loopFields($component, $fieldParsers)
+        {
+            $res = [];
+
+            foreach ($component->fields as $key => $field) {
+                $value = '';
+                if ($field->field == 'ForEach') {
+                    $subfields = $field->value;
+                    $value = [];
+
+                    foreach ($subfields as $subfield) {
+                        $value[] = loopFields($subfield, $fieldParsers);
+                    }
+                } else if (array_key_exists($field->field, $fieldParsers)) {
+                    $value = $fieldParsers[$field->field]($field->value);
+                } else {
+                    $value = $field->value;
+                }
+                $res[$key] = $value;
+            }
+            return $res;
+        };
+
+        foreach ($components as $component) {
+            $rawBlade = Storage::disk('components')->get($component->component . '.blade.php');
+            $rawBladeBody = YamlFrontMatter::parse($rawBlade)->body();
+            $compiledBlade = Blade::compileString($rawBladeBody);
+            $values = loopFields($component, $fieldParsers);
+
+            $rendered .= Aacms::toBlade($compiledBlade, $values);
+        }
+
+        return $rendered;
+    }
+
+    /**
+     * Function for rendering blade files with data
+     */
+    private static function toBlade($__php, $__data)
     {
         $obLevel = ob_get_level();
         ob_start();
--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -12,66 +12,15 @@ use Illuminate\Support\Facades\Blade;
 class TemplateController extends Controller
 {
     function index(){
-        $res = [];
-        $components = Storage::disk('components')->files('');
-        
-        foreach ($components as $component) {
-            $content = Storage::disk('components')->get($component);
-            $name = explode(".", $component, 2)[0];
-            $object = YamlFrontMatter::parse($content);
-            preg_match_all('/\$(\w*-?>?\[?\'?"?\]?+)/', $object->body(), $variables);
-            $res[$name] = [
-                'fields' => $object->matter(),
-                'variableNames' =>  $variables[1]
-            ];
-        }
+        $components = Aacms::getComponentList();
 
-        return Response::json($res, 200);
+        return Response::json($components, 200);
     }
     function preview(Request $request)
     {
-        $rendered = '';
         $components = json_decode($request->getContent());
-        $fieldParsers = [];
 
-        $fieldParsers = [
-            'RichText' => function () {
-                return 'rich text string';
-            }
-        ];
-
-        function loopFields($component, $fieldParsers)
-        {
-            $res = [];
-
-            foreach ($component->fields as $key => $field) {
-                $value = '';
-                if ($field->field == 'ForEach') {
-                    $subfields = $field->value;
-                    $value = [];
-
-                    foreach ($subfields as $subfield) {
-                        $value[] = loopFields($subfield, $fieldParsers);
-                    }
-                }
-                else if (array_key_exists($field->field, $fieldParsers)) {
-                    $value = $fieldParsers[$field->field]($field->value);
-                } else {
-                    $value = $field->value;
-                }
-                $res[$key] = $value;
-            }
-            return $res;
-        };
-
-        foreach ($components as $component) {
-            $rawBlade = Storage::disk('components')->get($component->component.'.blade.php');
-            $rawBladeBody = YamlFrontMatter::parse($rawBlade)->body();
-            $compiledBlade = Blade::compileString($rawBladeBody);
-            $values = loopFields($component, $fieldParsers);
-
-            $rendered .= Aacms::render($compiledBlade, $values);
-        }
+        $rendered = Aacms::render($components);
 
         return view('landing-page', ['rendered' => $rendered]);
     }
